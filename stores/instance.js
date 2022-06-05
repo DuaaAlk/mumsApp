05bfe071e490dfb4781eb04d9964e9eedebf95e7
DuaaAlk <engduaa2013@gmail.com>
2022-06-05 16:57:43 +0300

    this is top-navigator-profile

--- stores/instance.js
+++ stores/instance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const baseURL = "http://192.168.8.102:3000/";
+export const baseURL = "http://192.168.8.107:3000/";
 
 export const instance = axios.create({
   baseURL: `${baseURL}api`,
